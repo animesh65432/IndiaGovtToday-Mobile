423242e2e69f4084ee8a9ca266e71d3f6813385d
animesh65432 <kiranduttta234@gmail.com>
2026-01-30 20:50:04 +0530

    add some files

--- api/aleart.ts
+++
@@ -1,9 +0,0 @@
-import { Call } from "@/service/call"
-
-export const addthesubscribe = (Email: string) => Call({
-    path: "/addsubscribe",
-    method: "POST",
-    request: {
-        Email
-    }
-})
--- context/lan.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react"
-
-export const lanContext = createContext({
-    lan: "English",
-    setLan: (lan: string) => { }
-})
